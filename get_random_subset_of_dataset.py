e122cf12d2d0183087c0b8b3f1673bc5055f8a29
Konstantiss <konstantinos.michopoulos@gmail.com>
2022-11-28 20:21:13 +0200

    Add script to get random subset of dataset.

---
+++ get_random_subset_of_dataset.py
@@ -0,0 +1,22 @@
+import os
+import random
+import shutil
+
+
+def subset(src_dir, trg_dir, num_images):
+    file_list = os.listdir(src_dir)
+    random.shuffle(file_list)
+    files_to_copy = file_list[0:num_images]
+
+    for file in files_to_copy:
+        file_path = os.path.join(src_dir, file)
+        shutil.copy(file_path, trg_dir)
+
+train_dir = './Fakeddit/train/'
+reduced_train_dir = './Fakeddit/train_reduced'
+subset(train_dir, reduced_train_dir, 10000)
+
+test_dir = './Fakeddit/test/'
+reduced_test_dir = './Fakeddit/test_reduced'
+subset(test_dir, reduced_test_dir, 3000)
+
