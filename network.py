3be30ef7180217d6108e42b58573259468b34096
konstantiss <konstantinos.michopoulos@gmail.com>
2022-11-29 13:07:24 +0200

    Fix progress bar.

--- network.py
+++ network.py
@@ -15,12 +15,12 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 num_classes = 2
 num_epochs = 1
 batch_size = 24
-learning_rate = 0.01
+learning_rate = 0.1
 
 train_transforms = transforms.Compose([
     transforms.Resize((224, 224)),
-    transforms.Lambda(lambda x: x.repeat(3, 1, 1) if x.shape[0] < 3 else x),  # convert grayscale to RGB
-    transforms.Lambda(lambda x: x[:3] if x.shape[0] > 3 else x),  # convert 4 channels to 3
+    transforms.Lambda(lambda x: x.repeat(3, 1, 1) if x.shape[0] < 3 else x),  # convert images to 3 channels
+    transforms.Lambda(lambda x: x[:3] if x.shape[0] > 3 else x),  # convert images to 3 channels
     transforms.RandomHorizontalFlip(p=0.5),
 ])
 
@@ -49,7 +49,6 @@ train_loader = torch.utils.data.DataLoader(
 valid_loader = torch.utils.data.DataLoader(
     valid_data, batch_size=batch_size, shuffle=True)
 
-#model = ResNet(ResidualBlock, [3, 4, 6, 3]).to(device)
 model = ResNet50(num_classes=2).to(device)
 
 # Loss and optimizer
@@ -66,7 +65,7 @@ losses_validate = []
 start_time = time.time()
 
 for epoch in range(num_epochs):
-    with tqdm(enumerate(train_loader), unit="batch") as tepoch:
+    with tqdm(enumerate(train_loader), unit="batch", total=len(train_loader)) as tepoch:
         for i, (images, labels) in tepoch:
             tepoch.set_description(f"Epoch {epoch}")
             # Move tensors to the configured device
@@ -94,22 +93,23 @@ for epoch in range(num_epochs):
     with torch.no_grad():
         total_correct = 0
         total = 0
-        for images, labels in valid_loader:
-            images = images.to(device)
-            labels = labels.to(device)
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            losses_validate.append(loss.item())
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct = (predicted == labels).sum().item()
-            total_correct += correct
-            accuracy = correct / batch_size
-            accuracies_validate.append(accuracy)
-            del images, labels, outputs
+        with tqdm(valid_loader, unit="batch", total=len(valid_loader)) as tepoch:
+            for images, labels in tepoch:
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                losses_validate.append(loss.item())
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct = (predicted == labels).sum().item()
+                total_correct += correct
+                accuracy = correct / batch_size
+                accuracies_validate.append(accuracy)
+                del images, labels, outputs
 
-        total_accuracy = 100 * total_correct / total
-        print('Accuracy of the network on the validation images: {} %'.format(100 * total_accuracy))
+            total_accuracy = 100 * total_correct / total
+            print('Accuracy of the network on the validation images: {} %'.format(total_accuracy))
 
 print('Total execution time: {:.4f} minutes'
       .format((time.time() - start_time) / 60))
@@ -118,7 +118,7 @@ plt.figure(figsize=(10,5))
 plt.title("Training and Validation Loss")
 plt.plot(losses_validate,label="val")
 plt.plot(losses_train,label="train")
-plt.xlabel("iterations")
+plt.xlabel("Epoch")
 plt.ylabel("Loss")
 plt.legend()
 plt.show()
@@ -127,7 +127,7 @@ plt.figure(figsize=(10,5))
 plt.title("Training and Validation accuracy")
 plt.plot(accuracies_validate,label="val")
 plt.plot(accuracies_train,label="train")
-plt.xlabel("iterations")
-plt.ylabel("accuracy")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
 plt.legend()
 plt.show()
